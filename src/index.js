53b96cd7660f723b530a3a0b9d98c954754d7c5a
mku777 <mkm.kuz777@gmail.com>
2022-10-19 13:53:24 +0300

    upd index

--- src/index.js
+++ src/index.js
@@ -5,3 +5,54 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
+const gallery = document.querySelector('.gallery');
+const endText = document.querySelector('.end-text');
+const searchForm = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let currentPage = 1;
+let currentHits = 0;
+let searchQuery = '';
+
+searchForm.addEventListener('submit', submitSearchForm);
+
+async function submitSearchForm(event) {
+  event.preventDefault();
+  searchQuery = event.currentTarget.searchQuery.value.trim();
+  currentPage = 1;
+
+  if (searchQuery === '') {
+    Notify.info("Sorry, you need to write something in the form")
+    return;
+  }
+
+  const response = await fetch(searchQuery, currentPage);
+  currentHits = response.hits.length;
+
+  if (response.totalHits > 40) {
+    loadMoreBtn.classList.remove('is-hidden');
+    endText.classList.add('is-hidden');
+  } else {
+    loadMoreBtn.classList.add('is-hidden');
+    endText.classList.remove('is-hidden');
+  }
+
+  try {
+    if (response.totalHits > 0) {
+      Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      gallery.innerHTML = '';
+      markupImageCard(response.hits, gallery);
+      lightbox.refresh();
+    }
+
+    if (response.totalHits === 0) {
+      gallery.innerHTML = '';
+      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      loadMoreBtn.classList.add('is-hidden');
+      endText.classList.add('is-hidden');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
